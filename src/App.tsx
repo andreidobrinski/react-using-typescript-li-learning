6f6e498458dfcfaa620e2f7633125980bbea89b4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-15 09:53:14 -0400

    add functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
   // Basic types
@@ -28,6 +29,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Message />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           The value {firstValue} is of {typeof firstValue} type.
