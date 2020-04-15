44e5778b746e2e8f3351a1e60ad4c2d302c2e856
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-15 09:45:04 -0400

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let firstValue: string = 'name';
+  let secondValue: number = 42;
+  let thirdValue: boolean = true;
+  let fourthValue: number[] = [2,3,4];
+  let fifthValue: Array<string> = ['2', '3', '4'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The value {firstValue} is of {typeof firstValue} type.
+          The value {secondValue} is of {typeof secondValue} type.
+          The value {thirdValue} is of {typeof thirdValue} type.
+          The value {fourthValue} is of {typeof fourthValue} type.
+          The value {fifthValue} is of {typeof fifthValue} type.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
